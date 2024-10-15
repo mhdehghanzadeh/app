4afa2e8d39db058f3fe8d7883dcfcf4c1390f212
mhdehghanzadeh <mhdehghanzadeh@yahoo.com>
2024-10-15 19:54:20 +0330

    test 2

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -3,7 +3,7 @@
     <div class="mb-4">
       <Link class="group flex items-center py-3" href="/">
         <icon name="dashboard" class="mr-2 w-4 h-4" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard2</div>
+        <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
       </Link>
     </div>
     <div class="mb-4">
